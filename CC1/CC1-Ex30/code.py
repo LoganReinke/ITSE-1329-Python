bed4841b8e2ecaa5f6e15ae683a6c5f161beb272
Logan <Logan@email.com>
2019-09-11 13:43:06 +0000

    finished 28 to 31

--- CC1/CC1-Ex30/code.py
+++ CC1/CC1-Ex30/code.py
@@ -1,1 +1,3 @@
 zen_of_python = '     Readability counts     '
+lead = zen_of_python.strip()
+print(lead)
--- CC1/CC1-Ex31/code.py
+++ CC1/CC1-Ex31/code.py
@@ -1,1 +1,5 @@
 str = 'X-DSPAM-Confidence:0.8475'
+locate = str.find(":")
+end = str.find("5", locate)
+final = float(str[locate+1 : end+1])
+print (final)
